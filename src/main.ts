77589fd9a1fe3bf36a0263125bc5edafe169bb13
jhony-24 <jhonyvegacuya24@gmail.com>
2021-03-13 17:23:05 -0500

    Add necessary packages

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import i18n from "./i18n";
 
 createApp(App)
+  .use(i18n)
   .use(router)
   .mount("#app");
