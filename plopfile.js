7c7120c1355d7a6ba5d4a3e4c6a721d9735cc7cc
jhony-24 <jhonyvegacuya24@gmail.com>
2021-03-18 08:54:54 -0500

    Add plop template

--- plopfile.js
+++ plopfile.js
@@ -1,7 +1,10 @@
-module.exports = plop => {
+module.exports = (
+  /** @type {import('plop').NodePlopAPI} */
+  plop
+) => {
   plop.setGenerator("basics", {
     description: "this is a skeleton plopfile",
     prompts: [], // array of inquirer prompts
-    actions: [] // array of actions
+    actions: [] // array of actions,
   });
 };
